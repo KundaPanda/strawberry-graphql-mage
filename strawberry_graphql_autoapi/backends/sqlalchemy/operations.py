9b437b9fdc2d2b27c54d86553ff4b2da93f6555c
Vojtěch Dohnal <vojdoh@gmail.com>
2021-09-19 19:21:29 +0200

    Update SQLAlchemy backend

--- strawberry_graphql_autoapi/backends/sqlalchemy/operations.py
+++ strawberry_graphql_autoapi/backends/sqlalchemy/operations.py
@@ -1,6 +1,6 @@
 import dataclasses
 from functools import partial
-from typing import List, Type, Any, Union
+from typing import List, Type, Any, Union, Dict
 
 from sqlalchemy import select, delete, and_, or_
 from sqlalchemy.orm import DeclarativeMeta, Session
@@ -17,6 +17,19 @@ def _build_pk_query(model: Type[Union[DeclarativeMeta, IEntityModel]], data: Any
     return and_(*(getattr(model, key) == attribute_getter(key) for key in pk))
 
 
+def _build_multiple_pk_query(model: Type[Union[DeclarativeMeta, IEntityModel]], data: List[Any]):
+    return or_(
+        _build_pk_query(model, {k: getattr(entry._primary_key, k) for k in model.get_primary_key()}) for entry in data)
+
+
+def _get_model_pk_values(model: Type[Union[DeclarativeMeta, IEntityModel]], data: DeclarativeMeta):
+    return tuple(getattr(data, key) for key in model.get_primary_key())
+
+
+def _get_dict_pk_values(model: Type[Union[DeclarativeMeta, IEntityModel]], data: Dict[str, Any]):
+    return tuple(data.get(key) for key in model.get_primary_key())
+
+
 def create_(session: Session, model: Type[DeclarativeMeta], data: List[Any]):
     models = [model(**dataclasses.asdict(create_type)) for create_type in data]
     session.add_all(models)
@@ -27,16 +40,14 @@ def create_(session: Session, model: Type[DeclarativeMeta], data: List[Any]):
 
 def update_(session: Session, model: Type[Union[DeclarativeMeta, IEntityModel]], data: List[Any]):
     input_values = model.get_attributes(GraphQLOperation.CREATE_ONE)
-    entities = {en.id: en for en in
-                session.execute(
-                    select(model)
-                        .where(model.id.in_((e._primary_key.id for e in data)))
-                ).scalars()}
+    pk_filter = _build_multiple_pk_query(model, data)
+    entities = {_get_model_pk_values(model, en): en for en in session.execute(select(model).where(pk_filter)).scalars()}
     for entry in data:
         for column in input_values:
-            setattr(entities[entry._primary_key.id], column,
+            model_instance = entities[_get_dict_pk_values(model, entry)]
+            setattr(model_instance, column,
                     getattr(entry, column,
-                            getattr(entities[entry._primary_key.id], column)))
+                            getattr(model_instance, column)))
     session.add_all(entities.values())
     session.commit()
     [session.refresh(e) for e in entities.values()]
@@ -44,8 +55,7 @@ def update_(session: Session, model: Type[Union[DeclarativeMeta, IEntityModel]],
 
 
 def delete_(session: Session, model: Type[Union[DeclarativeMeta, IEntityModel]], data: List[Any]):
-    pk_q = or_(
-        _build_pk_query(model, {k: getattr(entry._primary_key, k) for k in model.get_primary_key()}) for entry in data)
+    pk_q = _build_multiple_pk_query(model, data)
     statement = delete(model).where(pk_q)
     r = session.execute(statement)
     session.commit()
@@ -53,7 +63,7 @@ def delete_(session: Session, model: Type[Union[DeclarativeMeta, IEntityModel]],
 
 
 def retrieve_(session: Session, model: Type[DeclarativeMeta], data: Any):
-    return session.query(model).get(data._primary_key.id)
+    return session.query(model).get(_get_model_pk_values(model, data))
 
 
 def list_(session: Session, model: Type[DeclarativeMeta], data: Any):
