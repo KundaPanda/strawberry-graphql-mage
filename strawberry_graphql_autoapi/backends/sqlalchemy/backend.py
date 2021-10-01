b2b761a9db66ab142747cc62d5384c3764d80c44
Vojtěch Dohnal <vojdoh@gmail.com>
2021-10-01 14:35:28 +0200

    Major fixes for SQLA related joins

--- strawberry_graphql_autoapi/backends/sqlalchemy/backend.py
+++ strawberry_graphql_autoapi/backends/sqlalchemy/backend.py
@@ -2,11 +2,12 @@ from functools import lru_cache
 from typing import Any, Type, Dict, Tuple, Set, Union, Optional, List
 
 from sqlalchemy import inspect, Integer, String
-from sqlalchemy.orm import ColumnProperty, DeclarativeMeta, RelationshipProperty, Mapper, ONETOMANY, MANYTOMANY, \
+from sqlalchemy.orm import ColumnProperty, RelationshipProperty, Mapper, ONETOMANY, MANYTOMANY, \
     sessionmaker
 from sqlalchemy.orm.util import AliasedInsp
 from strawberry.types import Info
 
+from strawberry_graphql_autoapi.backends.sqlalchemy.models import _SQLAlchemyModel
 from strawberry_graphql_autoapi.backends.sqlalchemy.operations import list_, create_, retrieve_, update_, delete_
 from strawberry_graphql_autoapi.core.backend import DataBackendBase
 from strawberry_graphql_autoapi.core.types import IEntityModel, GraphQLOperation
@@ -22,14 +23,14 @@ class SQLAlchemyBackend(DataBackendBase):
         self._session = session_maker
 
     @staticmethod
-    def _remove_polymorphic_cols(model: Type[Union[IEntityModel, DeclarativeMeta]], cols: List[str]) -> List[str]:
+    def _remove_polymorphic_cols(model: Type[Union[IEntityModel, _SQLAlchemyModel]], cols: List[str]) -> List[str]:
         inspection: Mapper = inspect(model)
         if inspection.polymorphic_on is None:
             return cols
         return [c for c in cols if c != inspection.polymorphic_on.key]
 
     @staticmethod
-    def _remove_pk_cols(model: Type[Union[IEntityModel, DeclarativeMeta]], cols: List[str]):
+    def _remove_pk_cols(model: Type[Union[IEntityModel, _SQLAlchemyModel]], cols: List[str]):
         return [c for c in cols if c not in model.get_primary_key()]
 
     @staticmethod
@@ -40,7 +41,7 @@ class SQLAlchemyBackend(DataBackendBase):
             return True
         return all((c.nullable for c in col.local_columns))
 
-    def get_attributes(self, model: Type[Union[IEntityModel, DeclarativeMeta]],
+    def get_attributes(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]],
                        operation: Optional[GraphQLOperation] = None) -> List[str]:
         inspection = inspect(model)
         all_ = [a.key for a in (inspection.mapper.attrs if isinstance(inspection, AliasedInsp) else inspection.attrs)]
@@ -67,17 +68,17 @@ class SQLAlchemyBackend(DataBackendBase):
         return python_type
 
     @lru_cache
-    def get_attribute_type(self, model: Type[Union[IEntityModel, DeclarativeMeta]], attr: str) -> Type:
+    def get_attribute_type(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]], attr: str) -> Type:
         inspection = inspect(model)
         col: Union[ColumnProperty, RelationshipProperty] = \
             getattr(inspection.mapper.attrs if isinstance(inspection, AliasedInsp) else inspection.attrs, attr)
         return self._get_attribute_type(col)
 
     @lru_cache
-    def get_attribute_types(self, model: Type[Union[IEntityModel, DeclarativeMeta]]) -> Dict[str, Type]:
+    def get_attribute_types(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]]) -> Dict[str, Type]:
         return {attr: self.get_attribute_type(model, attr) for attr in self.get_attributes(model)}
 
-    def get_primary_key(self, model: Type[Union[IEntityModel, DeclarativeMeta]]) -> Tuple:
+    def get_primary_key(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]]) -> Tuple:
         return tuple(a.key for a in inspect(model).primary_key)
 
     def get_parent_class_name(self, model: Type['IEntityModel']) -> Optional[str]:
@@ -86,12 +87,12 @@ class SQLAlchemyBackend(DataBackendBase):
             return [m.class_.__name__ for m in inspection.polymorphic_map.values()
                     if m.local_table == inspection.polymorphic_on.table][0]
 
-    def get_children_class_names(self, model: Type[Union['IEntityModel', DeclarativeMeta]]) -> Optional[List[str]]:
+    def get_children_class_names(self, model: Type[Union['IEntityModel', _SQLAlchemyModel]]) -> Optional[List[str]]:
         inspection = inspect(model)
         if inspection.polymorphic_on is not None and inspection.polymorphic_on.table == model.__table__:
             return [m.class_.__name__ for m in inspection.polymorphic_map.values()]
 
-    def get_operations(self, model: Type[Union[IEntityModel, DeclarativeMeta]]) -> Set[GraphQLOperation]:
+    def get_operations(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]]) -> Set[GraphQLOperation]:
         return {GraphQLOperation(i) for i in range(1, 9)}
 
     def _build_selection(self, field, selection=None):
@@ -99,11 +100,13 @@ class SQLAlchemyBackend(DataBackendBase):
             selection = {}
         for subfield in field.selections:
             if subfield.selections:
+                if not hasattr(subfield, 'name'):
+                    continue
                 selection[subfield.name] = {}
                 self._build_selection(subfield, selection[subfield.name])
         return selection
 
-    def resolve(self, model: Type[Union[IEntityModel, DeclarativeMeta]], operation: GraphQLOperation, info: Info,
+    def resolve(self, model: Type[Union[IEntityModel, _SQLAlchemyModel]], operation: GraphQLOperation, info: Info,
                 data: Any) -> Any:
         with self._session() as s:
             for field in info.selected_fields:
--- strawberry_graphql_autoapi/backends/sqlalchemy/tests/example_app/schema.py
+++ strawberry_graphql_autoapi/backends/sqlalchemy/tests/example_app/schema.py
@@ -1,58 +1,44 @@
 import enum
-from abc import ABC
 from typing import Union, Tuple, Type
 
 from inflection import underscore
 from sqlalchemy import Column, Integer, ForeignKey, String, Enum, Float, create_engine
-from sqlalchemy.orm import declared_attr, relationship, as_declarative, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
 
-from strawberry_graphql_autoapi.backends.sqlalchemy.backend import SQLAlchemyBackend
-from strawberry_graphql_autoapi.core.models import EntityModel
+from strawberry_graphql_autoapi.backends.sqlalchemy.models import create_base_entity
 from strawberry_graphql_autoapi.core.schema import SchemaManager
-from strawberry_graphql_autoapi.core.types import IEntityModel
 
-engine = create_engine('sqlite:///', echo=True)
-backend = SQLAlchemyBackend(session_maker=sessionmaker(bind=engine))
+engine = create_engine('sqlite:///test.db', echo=True)
 
-
-@as_declarative()
-class Base:
-    @declared_attr
-    def __tablename__(self):
-        return underscore(self.__name__)
-
-    __backend__ = backend
+Base = create_base_entity(sessionmaker(bind=engine))
 
 
-class BaseMeta(type(IEntityModel), type(Base)):
-    pass
-
-
-class AbstractModel(Base, EntityModel, metaclass=BaseMeta):
-    __abstract__ = True
-
-
-def make_fk(remote: Union[str, Type[Base]], remote_pk='id', optional=False, back_populates=None) \
-        -> Tuple[Column, relationship]:
+def make_fk(remote: Union[str, Type[Base]], remote_pk='id', optional=False, back_populates=None,
+            foreign_kwargs=None, rel_kwargs=None) -> Tuple[Column, relationship]:
     if not isinstance(remote, str):
         remote = remote.__tablename__
+    if foreign_kwargs is None:
+        foreign_kwargs = {}
+    if rel_kwargs is None:
+        rel_kwargs = {}
     remote_table = underscore(remote)
-    fk = Column(Integer, ForeignKey(f'{remote_table}.{remote_pk}'), nullable=optional)
-    rel = relationship(remote, back_populates=back_populates)
+    fk = Column(Integer, ForeignKey(f'{remote_table}.{remote_pk}'), nullable=optional, **foreign_kwargs)
+    rel = relationship(remote, back_populates=back_populates, foreign_keys=[fk], **rel_kwargs)
     return fk, rel
 
 
-class Weapon(AbstractModel):
+class Weapon(Base):
     id = Column(Integer, primary_key=True)
     owner_id, owner = make_fk('Entity', optional=True, back_populates='weapons')
     damage = Column(Integer, nullable=False)
     name = Column(String(30), nullable=True)
 
 
-class Entity(AbstractModel):
+class Entity(Base):
     id = Column(Integer, primary_key=True)
     weapons = relationship(Weapon, back_populates='owner')
     entity_class = Column(String, nullable=False)
+    submits_to_id, submits_to = make_fk('King', optional=True, back_populates='subjects')
 
     __mapper_args__ = {
         'polymorphic_on': entity_class,
@@ -85,7 +71,17 @@ class Archer(Entity):
     }
 
 
-schema = SchemaManager(Weapon, Entity, Mage, Archer).get_schema()
+class King(Entity):
+    id = Column(Integer, ForeignKey('entity.id'), primary_key=True)
+    subjects = relationship('Entity', back_populates='submits_to', foreign_keys='Entity.submits_to_id')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'king',
+        'inherit_condition': id == Entity.id
+    }
+
+
+schema = SchemaManager(Weapon, Entity, Mage, Archer, King).get_schema()
 
 if __name__ == '__main__':
     Base.metadata.create_all(bind=engine)
